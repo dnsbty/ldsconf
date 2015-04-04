020a8a291574b50cd10d2bb61c14962cab2a67ba
Dennis Beatty <dennis@dcbeatty.com>
2015-04-04 15:22:21 -0600

    require dependencies in index

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var twitter = require('twit');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -8,6 +11,6 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
