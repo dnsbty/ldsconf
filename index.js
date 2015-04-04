553b6e103015def1056e7dc97c3a527af9061f60
Dennis Beatty <dennis@dcbeatty.com>
2015-04-04 16:07:52 -0600

    try to fix heroku crashing

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
