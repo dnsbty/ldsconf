93175ee2365130d2ea54e9c50f3aee726e0905f7
Dennis Beatty <dennis@dcbeatty.com>
2015-04-05 14:27:43 -0600

    pause button

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,13 +1,36 @@
 $(function(){
+
+  var paused = false;
+
+  //bring in tweets
   var socket = io.connect(window.location.host);
   socket.on('new tweet', function (tweet) {
-    $('.tweets').prepend('<div class="tweet" id="'+tweet.id+'"></div>');
-    $('#'+tweet.id).append('<img class="profile-image" src="'+tweet.profile_image+'">')
-                   .append('<h2>'+tweet.user+'</h2>')
-                   .append('<p>'+tweet.text+'</p>');
+  	if (paused == false)
+  	{
+  	  $('.tweets').prepend('<div class="tweet" id="'+tweet.id+'"></div>');
+      $('#'+tweet.id).append('<img class="profile-image" src="'+tweet.profile_image+'">')
+                     .append('<h2>'+tweet.user+'</h2>')
+                     .append('<p>'+tweet.text+'</p>');
+  	}
+  });
+
+  //pause button handling
+  $('a[rel=pause]').on('click', function(){
+  	if (paused == false)
+  	{
+  	  paused = true;
+      $(this).html('<i class="fa fa-play"></i>');
+  	}
+  	else
+  	{
+      paused = false;
+      $(this).html('<i class="fa fa-pause"></i>')
+  	}
+  	
   });
 });
 
+//google analytics stuff
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
