f5eb3e9ce635d796d327a158b743c70124dae099
Dennis Beatty <dennis@dcbeatty.com>
2015-04-04 19:10:57 -0600

    put the comments back in

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+//load the config json file
 var fs = require('fs');
 var config = JSON.parse(fs.readFileSync('./config.json'));
 
+//create and configure the express app and server
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
@@ -9,6 +11,7 @@ var io = require('socket.io')(server);
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+//set up the twitter instance
 var Twit = require('twit');
 var twitter = new Twit({
   consumer_key: config.twitter.consumer_key,
@@ -17,10 +20,12 @@ var twitter = new Twit({
   access_token_secret: config.twitter.access_token_secret
 });
 
+//homepage
 app.get('/', function(request, response) {
   response.send('Hello world!');
 });
 
+//start the server listening for requests
 server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
