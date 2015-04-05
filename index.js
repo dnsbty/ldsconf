a95f3625f771b85d13d789091ab7a01c54f8d1f8
Dennis Beatty <dennis@dcbeatty.com>
2015-04-04 18:57:09 -0600

    get rid of the file stuff to see if that works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //load the config json file
-var fs = require('fs');
-var config = JSON.parse(fs.readFileSync('./config.json'));
+/*var fs = require('fs');
+var config = JSON.parse(fs.readFileSync('./config.json'));*/
 
 //create and configure the express app and server
 var express = require('express');
@@ -12,13 +12,13 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 //set up the twitter instance
-var Twit = require('twit');
+/*var Twit = require('twit');
 var twitter = new Twit({
   consumer_key: config.twitter.consumer_key,
   consumer_secret: config.twitter.consumer_secret,
   access_token: config.twitter.access_token,
   access_token_secret: config.twitter.access_token_secret
-});
+});*/
 
 //homepage
 app.get('/', function(request, response) {
@@ -26,6 +26,6 @@ app.get('/', function(request, response) {
 });
 
 //start the server listening for requests
-app.listen(app.get('port'), function() {
+server.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
