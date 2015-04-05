14dd735e4c72e87221faced550903d315e5e82f0
Dennis Beatty <dennis@dcbeatty.com>
2015-04-05 14:01:27 -0600

    basic mobile styling

--- public/scripts/global.js
+++ public/scripts/global.js
@@ -1,11 +1,9 @@
 $(function(){
   var socket = io.connect(window.location.host);
   socket.on('new tweet', function (tweet) {
-    console.log(tweet);
-    $('.container').prepend('<div class="tweet" id="'+tweet.id+'"></div>');
+    $('.tweets').prepend('<div class="tweet" id="'+tweet.id+'"></div>');
     $('#'+tweet.id).append('<img class="profile-image" src="'+tweet.profile_image+'">')
                    .append('<h2>'+tweet.user+'</h2>')
                    .append('<p>'+tweet.text+'</p>');
-    //socket.emit('my other event', { my: 'data' });
   });
 });
